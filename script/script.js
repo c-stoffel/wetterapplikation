cf3b294b7606a881bcc36d3edae25f2e78199a1c
c-stoffel <c.stoffel@me.com>
2014-05-23 10:21:27 +0200

    first try
    ok

--- script/script.js
+++ script/script.js
@@ -8,9 +8,12 @@ function success(pos) {
   var crd = pos.coords;
 
   console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
+  // console.log('Latitude : ' + crd.latitude);
+  // console.log('Longitude: ' + crd.longitude);
+  // console.log('More or less ' + crd.accuracy + ' meters.');
+  $().text(crd.latitude);
+  $().text(crd.longitude);
+  $().text(crd.accuracy);
 };
 
 function error(err) {
